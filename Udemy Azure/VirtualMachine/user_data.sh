3a3f1f1f2c9595a320e6cc37cd23dee9588ea72b
Mary <marisol.pereira.goncalves@gmail.com>
2025-01-13 19:49:37 +0100

    comandos

--- Udemy Azure/VirtualMachine/user_data.sh
+++ Udemy Azure/VirtualMachine/user_data.sh
@@ -1,5 +1,15 @@
-#/bin/bash
+#!/bin/bash
 sudo su
-apt-get -y update
-apt-get -y install nginx
-echo "<h1>Hola Mundo desde $(hostname)</h1" >/var/www/html/index.html
+apt-get -y update                      # Update packages
+apt-get -y install nginx               # Install Nginx
+echo "<h1>Hello World from $(hostname)</h1>" > /var/www/html/index.html
+systemctl restart nginx                # Restart Nginx to apply changes
+
+//////////////////////////////////////////////////////////////////////////////
+ssh para que funcione
+
+sudo mkdir /var/www/html/video
+sudo sh -c 'echo "Esto son mis videos/imagenes" > /var/www/html/video/index.html
+
+luego vas y das la direccion ip seria
+ip/video o imagenes // deberia mostrarte la pagina
