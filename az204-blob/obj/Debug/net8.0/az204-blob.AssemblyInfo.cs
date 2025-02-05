03eeedca2096073cfa60183470af9413ad4c212c
Mary <marisol.pereira.goncalves@gmail.com>
2025-02-05 12:02:53 +0100

    ejemplo mi proyecto

--- az204-blob/obj/Debug/net8.0/az204-blob.AssemblyInfo.cs
+++ az204-blob/obj/Debug/net8.0/az204-blob.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5112dd3101233b3ea3f1f25191578fcfd9de3ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043208cee3db3afd8479115b4f10bb3bba9ab5a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("az204-blob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
