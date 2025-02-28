a30e26a50bf927ab5c1af634b0151dace5d14d78
Mary <marisol.pereira.goncalves@gmail.com>
2025-02-28 13:59:21 +0100

    muestra c1

--- C#/CsharpProjects/TestProject/Program.cs
+++ C#/CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+
+// Para Crear un proyecto  es:  dotnet new console -o ./CsharpProjects/TestProject
+// Para compilar y ejecutar el proyecto: dotnet run
+// Para compilarlo: dotnet build
+// Para instalar las dependencias de NuGet: dotnet add package Microsoft.Azure.Cosmos -v 3.19.0
+
+
+
+// LLame a diferentes tipos de métodos en la biblioteca de clases .NET usando System.Random y System.Console
+
+// Ejercicio 1
+// Random dice = new Random();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// Ejercicio 2
+// int number = 7;
+// string text = "seven";
+
+// Console.WriteLine(number);
+// Console.WriteLine();
+// Console.WriteLine(text);
+
+// Ejercicio 3
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+// Ejercicio 4
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue= Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
+
+
--- C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4d911ebc3451ab1ffa0520eb24ef86d6147897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bff49c5e940e5f31b9456cf510393613691a991")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
