834fb6bbead507a9881be53d5da7d752388b5b44
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-10 15:21:16 +0100

    program ejemplos 1

--- C#/CsharpProjects/TestProject/Program.cs
+++ C#/CsharpProjects/TestProject/Program.cs
@@ -33,11 +33,72 @@
 // Console.WriteLine($"Third roll: {roll3}");
 
 // Ejercicio 4
-int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue;
 
-largerValue= Math.Max(firstValue, secondValue);
-Console.WriteLine(largerValue);
+// largerValue= Math.Max(firstValue, secondValue);
+// Console.WriteLine(largerValue);
 
+//Ejercicio 5 Matrices
 
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// // fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// Ejercicio 6 Matrices
+// string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// Ejercicio 7 Matrices forech
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// Ejercicio 8 Matrices forech
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+// Ejercicio 9 Matrices
+
+ // Declaración del array con identificadores de pedidos
+string[] fraudulentOrderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+Console.WriteLine("Pedidos fraudulentos que comienzan con 'B':");
+
+// Recorremos el array y filtramos los que comienzan con "B"
+        foreach (string orderID in fraudulentOrderIDs)
+        {
+            if (orderID.StartsWith("B"))
+            {
+                Console.WriteLine(orderID);
+            }
+        }
+    
--- C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bff49c5e940e5f31b9456cf510393613691a991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9529f6a7f01acbb5861f1cda757163a7d29663d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
