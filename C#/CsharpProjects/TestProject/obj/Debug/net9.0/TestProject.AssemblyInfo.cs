691c085847d5712bf3c842f15d3bbfe674c4a467
Mary <marisol.pereira.goncalves@gmail.com>
2025-06-03 23:19:20 +0200

    ejercicio

--- C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14b0f6913c5bf8081cf8bc4f154ed32205aa5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa5a13b8f8c04aada377ef7b1d059001f9553c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Microsoft Graph/MiProyectoGraph/obj/Debug/net9.0/MiProyectoGraph.AssemblyInfo.cs
+++ Microsoft Graph/MiProyectoGraph/obj/Debug/net9.0/MiProyectoGraph.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiProyectoGraph")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14b0f6913c5bf8081cf8bc4f154ed32205aa5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aa5a13b8f8c04aada377ef7b1d059001f9553c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiProyectoGraph")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiProyectoGraph")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
